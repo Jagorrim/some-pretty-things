4d54abb8ca62f0a3863080fe2d13a1de819aee94
Jagorrim <96289746+Jagorrim@users.noreply.github.com>
2024-04-27 22:47:05 +0500

    Add files via upload

--- datatree.py
+++ datatree.py
@@ -3,7 +3,7 @@ from copy import deepcopy
 
 class DataTree:
     # this is data structure for strings.
-    # It's scalable to be structure for any collection of hashable elements
+    # It's actually scalable to be structure for any collection of hashable elements
     def __init__(self) -> None:
         self.__data = {}
 
@@ -15,45 +15,44 @@ class DataTree:
         # adding new string to tree
 
         cur_level = self.__data
-        cur_item_index = 0
 
-        while cur_item_index < len(string):
-            if string[cur_item_index] not in cur_level:
-                cur_level[string[cur_item_index]] = {}
+        for letter in string:
+            if letter not in cur_level:
+                cur_level[letter] = {}
+            cur_level = cur_level[letter]
 
-            cur_level = cur_level[string[cur_item_index]]
-            cur_item_index += 1
-
-        cur_level['is_end'] = True
+        if 'end_for' not in cur_level:
+            cur_level['end_for'] = 0
+        cur_level['end_for'] += 1
 
     def delete(self, string: str) -> None:
-        # deleting item from tree
+        # deleting string from tree
         levels = [self.__data]
-
         cur_level = self.__data
-        cur_item_index = 0
 
-        is_string_seq_end = False
-
-        while cur_item_index < len(string):
-            if string[cur_item_index] not in cur_level:
+        for letter in string:
+            if letter not in cur_level:
                 raise ValueError(f'Tree {self} does not contain {string}!')
 
-            if len(cur_level[string[cur_item_index]]) == 1 and 'is_end' in cur_level[string[cur_item_index]]:
-                is_string_seq_end = True
-                break
-
-            cur_level = cur_level[string[cur_item_index]]
+            cur_level = cur_level[letter]
             levels.append(cur_level)
-            cur_item_index += 1
 
-        if is_string_seq_end:
+        # if this level is end for many strings
+        if 'end_for' in cur_level and cur_level['end_for'] > 1:
+            cur_level['end_for'] -= 1
+
+        # if this level is end for only one string, but there are strings that longer than it
+        elif 'end_for' in cur_level and cur_level['end_for'] == 1 and len(cur_level) > 1:
+            del cur_level['end_for']
+
+        # if this level is end for only one string and there are no string that longer than it
+        elif 'end_for' in cur_level and cur_level['end_for'] == 1 and len(cur_level) == 1:
             end_counter = 0
 
             for index in range(len(string) - 1, -1, -1):
                 if len(levels[index][string[index]]) == 1 \
-                        and 'is_end' in levels[index][string[index]] \
-                        and end_counter == 0:
+                        and 'end_for' in levels[index][string[index]] \
+                        and end_counter == 0:  # we can not delete more than one string
                     end_counter += 1
                     del levels[index][string[index]]
                 elif len(levels[index][string[index]]) == 0:
@@ -74,17 +73,15 @@ class DataTree:
             return False
 
         cur_level = self.__data
-        cur_item_index = 0
 
-        while cur_item_index < len(string):
-            if string[cur_item_index] not in cur_level:
+        for letter in string:
+            if letter not in cur_level:
                 return False
+            cur_level = cur_level[letter]
 
-            cur_level = cur_level[string[cur_item_index]]
-            cur_item_index += 1
-
-        if 'is_end' in cur_level:
+        if 'end_for' in cur_level:
             return True
+        return False
 
     def __delitem__(self, key) -> None:
         self.delete(key)
@@ -97,45 +94,61 @@ if __name__ == '__main__':
     from time import time
     from random import randrange
 
-
-    max_num = 1_000_000_0
-
-    lst = [str(i) for i in range(0, max_num)]
+    #
+    # max_num = 1_000_000_0
+    #
+    # lst = [str(i) for i in range(0, max_num)]
+    #
+    # tree = DataTree()
+    #
+    # for i in lst:
+    #     tree.add(i)
+    #
+    # tree_time = 0
+    # list_time = 0
+    #
+    # test_count = 1000
+    #
+    # for _ in range(test_count):
+    #     num_to_search = randrange(0, max_num)
+    #
+    #     start1 = time()
+    #     res1 = str(num_to_search) in lst
+    #     finish1 = time()
+    #     # print(res1, finish1 - start1)
+    #     list_time += (finish1 - start1)
+    #
+    #     start2 = time()
+    #     res2 = str(num_to_search) in tree
+    #     finish2 = time()
+    #     # print(res2, finish2 - start2)
+    #     tree_time += (finish2 - start2)
+    # print(f'tree_time: {tree_time}')
+    # print(f'list_time: {list_time}')
 
     tree = DataTree()
 
-    for i in lst:
-        tree.add(i)
+    tree.add('abc')
+    tree.add('abcdeee')
+    tree.add('abcdeee')
+    tree.add('abcdeee')
 
-    tree_time = 0
-    list_time = 0
+    tree.add('abcdeee')
 
-    test_count = 1000
+    tree.add('abcd')
+    print('abcdeee' in tree)
+    print('ab' in tree)
 
-    for _ in range(test_count):
-        num_to_search = randrange(0, max_num)
-
-        start1 = time()
-        res1 = str(num_to_search) in lst
-        finish1 = time()
-        # print(res1, finish1 - start1)
-        list_time += (finish1 - start1)
-
-        start2 = time()
-        res2 = str(num_to_search) in tree
-        finish2 = time()
-        # print(res2, finish2 - start2)
-        tree_time += (finish2 - start2)
-    print(f'tree_time: {tree_time}')
-    print(f'list_time: {list_time}')
-
-    # tree = DataTree()
-    #
-    # tree.add('DADADAD')
-    # print(tree.get_tree())
-    # tree.add('DADADADA')
-    # print(tree.get_tree())
-    # print('DADADADA' in tree)
-    # tree.delete('DADADADA')
-    # print(tree.get_tree())
-    # print('DADADADA' in tree)
+    print(tree.get_tree())
+    del tree['abcdeee']
+    print(tree.get_tree())
+    del tree['abcdeee']
+    print(tree.get_tree())
+    del tree['abcdeee']
+    print(tree.get_tree())
+    del tree['abcdeee']
+    print(tree.get_tree())
+    del tree['abcd']
+    print(tree.get_tree())
+    del tree['abc']
+    print(tree.get_tree())
