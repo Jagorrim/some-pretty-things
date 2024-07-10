d3c9da77776aee4897d0c3f62c0cfcef9dd1d0c6
Jagorrim <96289746+Jagorrim@users.noreply.github.com>
2024-07-10 17:30:20 +0500

    Add files via upload
    Здесь реализован выбор наиболее близкого к медиане элемента в качестве опорного, но такая сортировка работает гораздо медленнее той, что делает это рандомно

--- qsort.py
+++ qsort.py
@@ -1,4 +1,71 @@
 from random import randrange
+from math import ceil, floor
+
+
+def get_median(data: list, ) -> int:
+    base_index = randrange(0, len(data))
+    base_elem = data[base_index]
+
+    less = []
+    greater = []
+
+    meet_base_elem = False
+
+    for i in data:
+        if i < base_elem:
+            less.append(i)
+        else:
+            if i == base_elem and meet_base_elem:
+                greater.append(i)
+
+            elif i == base_elem and not meet_base_elem:
+                meet_base_elem = True
+
+            else:
+                greater.append(i)
+
+    if len(less) == floor(len(data) / 2) or len(less) == ceil(len(data) / 2):
+        return base_index
+
+    if len(less) > ceil(len(data) / 2):
+        return get_median(less)
+
+    if len(less) < floor(len(data) / 2):
+        return len(less) + get_median(greater)
+
+
+# actually it works so slower than qsort that chose pivot item randomly (idk why)
+def _qsort(data: list) -> list:
+    if len(data) == 0 or len(data) == 1:
+        return data
+
+    if len(data) == 2:
+        if data[0] > data[1]:
+            data[0], data[1] = data[1], data[0]
+        return data
+
+    base_index = get_median(data)
+    base_elem = data[base_index]
+
+    less = []
+    greater = []
+
+    meet_base_elem = False
+
+    for i in data:
+        if i < base_elem:
+            less.append(i)
+        else:
+            if i == base_elem and meet_base_elem:
+                greater.append(i)
+
+            elif i == base_elem and not meet_base_elem:
+                meet_base_elem = True
+
+            else:
+                greater.append(i)
+
+    return _qsort(less) + [base_elem] + _qsort(greater)
 
 
 def qsort(data: list) -> list:
@@ -34,4 +101,21 @@ def qsort(data: list) -> list:
     return qsort(less) + [base_elem] + qsort(greater)
 
 
-print(qsort([5, 9, 1, 1, 2, 1, 2]))
+if __name__ == '__main__':
+    from time import time
+
+    _1_time = 0
+    _2_time = 0
+
+    for _ in range(100):
+        nums = [randrange(1, 101) for _ in range(1001)]
+        start = time()
+        _1 = _qsort(nums)
+        _1_time += (time() - start)
+
+        start = time()
+        _2 = qsort(nums)
+        _2_time += (time() - start)
+    print(_1_time)
+    print(_2_time)
+
